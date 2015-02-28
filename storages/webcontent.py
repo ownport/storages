253cc56e78e813226303b4970dcc8030d1cad96c
ownport <ownport@gmail.com>
2015-02-28 15:27:02 +0200

    [+] added web content

--- storages/webcontent.py
+++ storages/webcontent.py
@@ -1,23 +1,30 @@
+import json
 from packages import peewee
 
-class DictField(peewee.Fiels):
-    pass
+
+class DictField(peewee.Field):
+    db_field = 'dict'
+
+    def db_value(self, value):
+        return json.dumps(value)
+
+    def python_value(self, value):
+        return json.loads(value)
+
+peewee.Database.register_fields({'dict': 'blob'})
 
 
 webcontent_proxy = peewee.Proxy()
 class Scheme(peewee.Model):
 
-    content_id      = peewee.CharField(index=True)
-    headers         = peewee.DictField()
+    content_id      = peewee.CharField(index=True, unique=True)
+    headers         = DictField()
     payload         = peewee.CharField()
 
     class Meta:
         database = webcontent_proxy
         db_table = 'webcontent'
-        indexes = (
-            # unique index for content_id
-            (('content_id'), True),
-        )
+
 
     def as_dict(self):
 
@@ -26,3 +33,51 @@ class Scheme(peewee.Model):
             'headers': self.headers, 
             'payload': self.payload
         }
+
+
+class WebContent(object):
+
+    def __init__(self, path):
+        ''' path - path to metadata file (sqlite)
+        '''                
+        self._db = peewee.SqliteDatabase(path)
+        webcontent_proxy.initialize(self._db) 
+        if not Scheme.table_exists():
+            Scheme.create_table()
+
+    def put(self, content_id, headers={}, payload=None):
+        ''' store web content into database
+        '''
+        if not content_id:
+            raise RuntimeError('ContentId is not defined, %s' % content_id)
+
+        if not headers and not payload:
+            raise RuntimeError('Headers and payload are empty. Headers: %s, payload: %s' % (headers, payload))
+
+        with self._db.transaction():
+            try:
+                Scheme.create(**{'content_id': content_id, 'headers': headers, 'payload': payload})
+            except peewee.IntegrityError:
+                # TODO update metadata
+                pass
+        return self.get(content_id)
+
+
+    def get(self, content_id=None):
+        ''' get content_id
+        '''
+        if not content_id:
+            return [(r.content_id, r.headers, r.payload) \
+                for r in Scheme.select()]
+        else:
+            r = Scheme.select().where(Scheme.content_id == content_id)
+            if r.first():
+                return (r.first().content_id, r.first().headers, r.first().payload)
+            else:
+                return ()
+
+
+    def count(self):
+        ''' return count of records in database
+        '''
+        return Scheme.select().count()
---
+++ test/test_webcontent.py
@@ -0,0 +1,39 @@
+import unittest
+
+from storages import webcontent
+
+class SchemeTest(unittest.TestCase):
+
+    def test_create_scheme(self):
+        
+        scheme = webcontent.Scheme()
+        self.assertEqual(type(scheme), webcontent.Scheme)
+        self.assertEqual(scheme.as_dict(), {'content_id': None, 'headers': None, 'payload': None} )
+
+class WebContentTest(unittest.TestCase):
+
+    def test_create_webcontent(self):
+
+        content = webcontent.WebContent(':memory:')
+        self.assertTrue(type(content), webcontent.WebContent)
+
+    def test_store_webcontent(self):
+
+        content = webcontent.WebContent(':memory:')
+
+        content.put(u'1', {u'k1':u'v1'}, u'test')
+        self.assertEqual(content.count(), 1)
+
+        content.put(u'1', {u'k1':u'v1'}, u'test')
+        self.assertEqual(content.count(), 1)
+
+        self.assertEqual(content.get(), [(u'1', {u'k1': u'v1'}, u'test')])
+        self.assertEqual(content.get(u'1'), (u'1', {u'k1': u'v1'}, u'test'))
+        self.assertEqual(content.get(u'2'), ())
+
+
+    def test_put_webcontent(self):
+
+        content = webcontent.WebContent(':memory:')
+        self.assertRaises(RuntimeError, content.put, None, {})
+        self.assertRaises(RuntimeError, content.put, (None, None, None))
